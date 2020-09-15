a4011d9738dbfc9c4f45b795e2438a14f2a55775
Kevin Lydon <klydon@broadinstitute.org>
2020-09-15 15:54:34 -0400

    Added some basic test commands for pipelines and added commands for setting and storing config variables

--- src/carrot_cli/__main__.py
+++ src/carrot_cli/__main__.py
@@ -7,6 +7,9 @@ import click
 from .sub_command_1 import command as sub_command_1
 from .sub_command_2 import command as sub_command_2
 from .sub_command_3 import command as sub_command_3
+from .pipeline import command as pipeline
+from .config import manager as config_manager
+from .config import command as config
 
 # Version number is automatically set via bumpversion.
 # DO NOT MODIFY:
@@ -43,6 +46,9 @@ def main_entry(verbosity):
 
     log.configure_logging(verbosity)
 
+    # Make sure we have a config file
+    config_manager.create_config_dir_if_not_exists()
+
     # Log our command-line and log level so we can have it in the log file:
     LOGGER.info("Invoked by: %s", " ".join(argv))
     LOGGER.info("Log level set to: %s", logging.getLevelName(logging.getLogger().level))
@@ -58,6 +64,8 @@ def version():
 main_entry.add_command(sub_command_1.main)
 main_entry.add_command(sub_command_2.main)
 main_entry.add_command(sub_command_3.main)
+main_entry.add_command(pipeline.main)
+main_entry.add_command(config.main)
 
 
 if __name__ == "__main__":
